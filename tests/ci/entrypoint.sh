bb46f16ec06de893f76a5bc2793c37f84a4f3d73
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2020-07-27 21:27:26 +0200

    fixing codecov

--- tests/ci/entrypoint.sh
+++ tests/ci/entrypoint.sh
@@ -12,5 +12,5 @@ echo "Generating code coverage report..."
 gcovr -x -r .. -e ../tests/src -o codecov.xml
 echo "Uploading report to Codecov..."
 export CODECOV_TOKEN=$INPUT_CODECOV_TOKEN
-bash <(curl -s https://codecov.io/bash) -f codecov.xml
+curl -s https://codecov.io/bash | bash -f codecov.xml
 $TEST_STATUS && echo "All tests passed. Good job! Finishing..." && exit 0 || echo "Some tests failed. I'm going to crash now. Goodbye." && exit 1

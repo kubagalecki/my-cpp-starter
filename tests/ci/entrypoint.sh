7194146eb1b241b06eaa87c840ff7422d7f1ab4c
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2020-07-27 20:05:28 +0200

    fixing file paths

--- tests/ci/entrypoint.sh
+++ tests/ci/entrypoint.sh
@@ -5,6 +5,6 @@ cd build
 cmake ../tests
 make -j4
 ./tests && TEST_STATUS=true || TEST_STATUS=false
-gcovr -x -r .. -e ../tests/src
+gcovr -x -r .. -e ../tests/src -o codecov.xml
 bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
 $TEST_STATUS && exit 0 || exit 1

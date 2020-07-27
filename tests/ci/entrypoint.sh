e3cfae305568c5220c5b32116181c4a3ba00a635
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2020-07-27 21:40:54 +0200

    fixing codecov

--- tests/ci/entrypoint.sh
+++ tests/ci/entrypoint.sh
@@ -8,7 +8,6 @@ make -j4
 echo "Running tests..."
 ./tests && TEST_STATUS=true || TEST_STATUS=false
 $TEST_STATUS || echo "Tests failed. Delaying error until after code coverage is reported"
-echo "Generating code coverage report and uploading to Codecov"
-export CODECOV_TOKEN=$INPUT_CODECOV_TOKEN
-curl -s https://codecov.io/bash | bash -g "../tests/src/" -p "../"
+echo "Generating code coverage report and uploading to Codecov..."
+curl -s https://codecov.io/bash | bash -s -- -c -g ../tests/src/ -p .. -t $INPUT_CODECOV_TOKEN
 $TEST_STATUS && echo "All tests passed. Good job! Finishing..." && exit 0 || echo "Some tests failed. I'm going to crash now. Goodbye." && exit 1

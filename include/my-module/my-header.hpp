ce281c0acb96ecbd63ae5b266ea957e2fe81de57
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2020-09-14 16:39:14 +0200

    Added new features to be tested, split cmake build into 2 targets
    New features from C++17:
    - CTAD
    - std::reduce
    - execution policy

--- include/my-module/my-header.hpp
+++ include/my-module/my-header.hpp
@@ -1,11 +1,14 @@
 #include "utils/macros.h"
 
 #include <iostream>
+#include <vector>
+#include <execution>
 
 struct MyHeader
 {
     int                       getMember() { return member; }
     FORCE_DBG_GEN inline void untestedMemberFunction();
+    FORCE_DBG_GEN inline int  testCpp17Feature();
 
 private:
     static constexpr int computeMember() { return 42; }
@@ -18,4 +21,12 @@ inline void MyHeader::untestedMemberFunction()
     std::cin >> a;
     if (a == 1)
         std::cout << "You just typed in 1\n";
+}
+
+
+inline int testCpp17Feature()
+{
+    // Test that CTAD, std::reduce, and execution policies compile (new in C++17)
+    std::vector vec{1., 2., 3.};
+    return std::reduce(std::execution::seq, cbegin(vec), cend(vec));
 }
